6364b40abc3a4cbb59b4eabdf5940763459ac4b0
Lucas Yamada <{email}>
2025-04-13 00:31:35 -0300

    COMPLETED PROJECT v1.0

--- src/components/Caroussel.jsx
+++ src/components/Caroussel.jsx
@@ -1,11 +1,56 @@
-function Caroussel(){
-    return (
-        <>
-            <div clas>
+import { searchMovies, getPopularMovies } from "../Services/Api";
+import { useState, useEffect } from "react";
+import Movies from "./Movies";
 
-            </div>
-        </>
-    )
+function Caroussel() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        console.log(err);
+        setError("Failed to Load Movies...");
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadPopularMovies();
+  }, []);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (!searchQuery.trim()) return;
+    if (loading) return;
+
+    setLoading(true);
+
+    try {
+      const searchResults = await searchMovies(searchQuery);
+      setMovies(searchResults);
+      setError(null);
+    } catch (err) {
+      console.log(err);
+      setError("Failed to Load Movies...");
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <>
+      <div className="container-movies row">
+        {movies.map((movie) => (
+          <Movies movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Caroussel;
